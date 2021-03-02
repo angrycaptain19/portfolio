e82e03da33983f9ddb1c5b7cc55d2ee0f815c2d8
yuzoogusu <syouta@gmail.com>
2021-03-02 11:11:00 +0900

    4th commit

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile , Work
 
 admin.site.register(Profile)
+admin.site.register(Work)
 
 # Register your models here.
